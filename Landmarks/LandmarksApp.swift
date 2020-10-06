007211627bc50e313b17e9a8f99da9c1ce56eaa2
Flavia <flavia.brogle@hotmail.com>
2020-10-06 14:10:03 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Focus on 06.10.20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
